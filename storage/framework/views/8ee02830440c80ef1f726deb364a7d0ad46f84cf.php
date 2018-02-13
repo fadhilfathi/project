d23d7e41c630261f01ef6c43e6ccc0d8b2091b2c
rifqir4 <rifqiradifan@gmail.com>
2018-02-13 13:13:50 +0800

    Add files via upload

---
+++ storage/framework/views/8ee02830440c80ef1f726deb364a7d0ad46f84cf.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home | Kecamatan Rappocini</title>
+    <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div id="city-text">
+            <p>Kota Makassar</p>
+        </div>
+        <div id="work-time">
+            <ul>
+                <li>
+                    <p>kontak@kec-rappocini.id</p>
+                </li>
+                <li>
+                    <p>Senin - Jumat | 07:00 - 15:00</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+<section>
+    <div class="container">
+        <div id="header-mid">
+            <div id="phone">
+                <p>Phone</p>
+            </div>
+            <div id="branding">
+                <img src="../assets/images/kec-rappocini.png">
+            </div>
+            <div id="bbm">
+                <p>bbm</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container" id="navigasi">
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="index.html">Tentang</a></li>
+                <li><a href="index.html">Kelurahan</a></li>
+                <li><a href="index.html">Download</a></li>
+                <li><a href="index.html">Kontak</a></li>
+                <li><a href="index.html">PKK Kec</a></li>
+            </ul>
+        </nav>
+    </div>
+</section>
+<section>
+    <div class="carousel">
+        <div class="arrow" id="kiri"></div>
+        <div class="arrow" id="kanan"></div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div style="background-color: black; width: 100%; height: 100px;margin-top: 20px"></div>
+    </div>
+</section>
+
+<section>
+    <div class="container grid_dw">
+        <div class="download"></div>
+        <div class="download"></div>
+    </div>
+    <div class="container grid_c">
+        <div class="download"></div>
+        <div class="download"></div>
+    </div>
+</section>
+<div style="height: 800px"></div>
+</body>
+</html>
